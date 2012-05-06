251b08d36d9a5cbebcef5210b0550729071d4310
lgh <haolloyin@gmail.com>
2012-05-07 00:52:07 +0800

    add some useful funcs to check whether a user has authorize a social ID for sharing

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,9 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 
+DOUBAN_LOGIN_FLAG = "loged_with_douban"
+WEIBO_LOGIN_FLAG = "loged_with_weibo"
+
 class UUID(models.Model):
     user = models.ForeignKey(User)
     uuid = models.CharField(max_length=40)
@@ -24,27 +27,33 @@ class UserProfile(models.Model):
     share_to_weibo = models.BooleanField(default=False, help_text='发送微博')
     share_to_douban = models.BooleanField(default=False, help_text='发送豆瓣广播')
 
-    weibo_tokens = models.CharField(default='', max_length=100, null=True, blank=True)
-    douban_tokens = models.CharField(default='', max_length=100, null=True, blank=True)
+    weibo_tokens = models.CharField(default=None, max_length=100, null=True, blank=True)
+    douban_tokens = models.CharField(default=None, max_length=100, null=True, blank=True)
 
     def email(self):
         return self.user.email
 
-    def weibo_access_tokens(self):
-        if self.weibo_tokens != '':
-            tokens = self.weibo_tokens.split('_')
-            return {'access_token': tokens[0], 'expires_in': float(tokens[1])}
-        return None
+    def get_weibo_tokens_dict(self):
+        if self.weibo_tokens is None or self.weibo_tokens == '':
+            return None
+        tokens = self.weibo_tokens.split('@')
+        return {'access_token': tokens[0], 'expires_in': float(tokens[1])}
 
-    def douban_access_tokens(self):
-        if self.douban_tokens != '':
-            tokens = self.douban_tokens.split('_')
-            return {'access_token': tokens[0], 'token_secret': tokens[1]}
-        return None
+    def get_douban_tokens_dict(self):
+        if self.douban_tokens is None or self.douban_tokens == '':
+            return None
+        tokens = self.douban_tokens.split('@')
+        return {'access_token': tokens[0], 'token_secret': tokens[1]}
+
+    def has_weibo_oauth(self):
+        return (True if self.get_weibo_tokens_dict() else False)
+
+    def has_douban_oauth(self):
+        return (True if self.get_douban_tokens_dict() else False)
 
     def is_weibo_expires(self):
         return not self.weibo_tokens or \
-                time.time() > self.weibo_access_token()['expires_in']
+                time.time() > float(self.weibo_access_token()['expires_in'])
 
     def share_2_weibo(self):
         pass
