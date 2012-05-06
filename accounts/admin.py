d617cc7ac0a0528b33a879c702991d40963885c7
lgh <haolloyin@gmail.com>
2012-05-07 00:41:18 +0800

    rearrange the UserProfileAdmin display order

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,8 +5,6 @@ from iRead4Kindle.accounts.models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
     search_fields = ['user', 'douban_id', 'weibo_id', 'email', 'kindle_profile_url']
-    list_display = ['user', 'douban_id', 'weibo_id', 'email', \
-            'kindle_profile_url', 'share_to_weibo', 'share_to_douban', \
-            'weibo_tokens', 'douban_tokens']
+    list_display = ['user', 'douban_id', 'weibo_id', 'kindle_profile_url', 'share_to_weibo', 'weibo_tokens', 'share_to_douban', 'douban_tokens', 'email']
 
 admin.site.register(UserProfile, UserProfileAdmin)
