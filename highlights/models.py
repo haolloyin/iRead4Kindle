d4800f840642f803bde4e835f11a8c16d9407f69
lgh <haolloyin@gmail.com>
2012-05-08 10:52:15 +0800

    highlight's text use TextField, not CharField.

--- highlights/models.py
+++ highlights/models.py
@@ -6,11 +6,11 @@ from django.db import models
 class Highlight(models.Model):
     user = models.ForeignKey(User)
     url = models.CharField(max_length=128, blank=True, null=True)
-    text = models.CharField(max_length=1024)
+    text = models.TextField(max_length=1024)
     remark = models.CharField(max_length=128, blank=True, null=True)
     book = models.CharField(max_length=128, blank=True, null=True)
     author = models.CharField(max_length=128, blank=True, null=True)
-    added = models.DateTimeField()
+    added = models.DateTimeField(auto_now_add=True)
 
     is_shared_douban = models.BooleanField(default=False)
     is_shared_weibo = models.BooleanField(default=False)
@@ -20,10 +20,13 @@ class Highlight(models.Model):
         ordering = ["-added"]
 
     def __unicode__(self):
-        return self.text
+        return '%s: %s ...' % (self.user.username, self.text[:20])
 
     def author(self):
         return self.user.get_profile().get_kindle_name()
 
     def title(self):
         return self.text[:20]
+
+    def get_url(self):
+        return 'https://kindle.amazon.com%s' % self.url
